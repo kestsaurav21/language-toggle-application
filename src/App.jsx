24ec74172495f06a5a838bea4241e5c770d8f54b
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-14 01:01:59 +0530

    Language Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { LanguageProvider } from './context/LangaugeContext'
+import LanguageSwitcher from './components/LanguageSwitcher'
+import DisplayComponent from './components/DisplayComponent'
 
 function App() {
 
   return (
-    <>
-    </>
+    <LanguageProvider>
+      <LanguageSwitcher />
+      <DisplayComponent />
+    </LanguageProvider>
   )
 }
 
--- src/context/LangaugeContext.jsx
+++ src/context/LangaugeContext.jsx
@@ -1,7 +1,29 @@
-const { createContext } = require("react");
+import { createContext, useContext, useState } from 'react';
 
 
 const LanguageContext = createContext();
+
+
+const LanguageProvider = ( {children} ) => {
+
+    const [ language, setLanguage ] = useState('en');
+
+    const changeLanguage = ( lang ) => {
+        console.log("current language: ", language);
+        console.log("switched to: ", lang);
+        setLanguage(lang);
+    }
+
+
+    return (
+        <LanguageContext.Provider value={{language, changeLanguage}} >
+            {children}
+        </LanguageContext.Provider>
+    )
+
+}
+
+export { LanguageContext, LanguageProvider }
 
 
 
