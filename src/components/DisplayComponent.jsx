7767380d9829e09e6c95b6870180ff45ba9f52bf
kestsaurav21 <kestsaurav21@outlook.com>
2024-11-14 01:35:57 +0530

    DisplayComponent

--- src/components/DisplayComponent.jsx
+++ src/components/DisplayComponent.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import translation from "../mockData/translations.json"
+import availableLang from '../mockData/availableLang.json'
+import { LanguageContext } from '../context/LangaugeContext'
+
 
 const DisplayComponent = () => {
+
+  const { language } = useContext(LanguageContext)
+
+  const langName = availableLang.filter((lang) => lang.code == language);
+
   return (
-    <div>DisplayComponent</div>
+    <div>
+      
+        <h4>{translation[language].currentLang}: {langName[0].label} </h4>
+        <h2>{translation[language].greeting}</h2>
+        <h4>{translation[language].message}</h4>
+    </div>
   )
 }
 
